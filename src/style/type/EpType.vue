02b33c8db2829c80fbde932b57f781ad8cf63286
Eric Pitcock <accounts+git@ericpitcock.com>
2021-11-25 17:42:09 -0800

    type updates

--- src/style/type/EpType.vue
+++ src/style/type/EpType.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
-    This is Typography
+    <p class="font-size--jumbo">This is font size jumbo</p>
+    <p class="font-size--large">This is font size large</p>
+    <p class="font-size--body">This is a font size body</p>
+    <p class="font-size--default">This is a font size default</p>
+    <p class="font-size--small">This is a font size small</p>
+    <p class="font-size--tiny">This is a font size tiny</p>
+    <p class="font-size--body">{{ faker.lorem.paragraph(4) }}</p>
   </div>
 </template>
 
 <script>
+  import faker from 'faker'
+
   export default {
-    
+    data() {
+      return {
+        faker
+      }
+    } 
   }
 </script>
 
 <style lang="scss" scoped>
-
+  div > p + p {
+    margin-top: 3rem;
+  }
 </style>
