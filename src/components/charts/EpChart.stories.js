fd7fababaa26807bb31c822e3078221f5c9c2938
Eric Pitcock <accounts+git@ericpitcock.com>
2021-12-11 10:32:03 -0800

    finally working with scoped style

---
+++ src/components/charts/EpChart.stories.js
@@ -0,0 +1,137 @@
+import faker from 'faker'
+import EpContainer from '@/components/container/EpContainer'
+import EpChart from './EpChart'
+
+const container = () => {
+  return {
+    template: '<div style="padding: 30px;"><story/></div>'
+  }
+}
+
+const fakeArray = length => {
+  let arr = []
+  for (let i = 0; i < length; i++) {
+    arr.push(faker.datatype.number({ min: 0, max: 10000, precision: 0.1 }))
+  }
+  return arr
+}
+
+const fakeCitiesArray = length => {
+  let arr = []
+  for (let i = 0; i < length; i++) {
+    arr.push(faker.address.city())
+  }
+  return arr
+}
+
+const textColor = getComputedStyle(document.documentElement).getPropertyValue(
+  '--text-color'
+)
+
+const options = {
+  chart: {
+    // plotBackgroundColor: null,
+    // plotBorderWidth: null,
+    // plotShadow: false,
+    styledMode: true,
+    type: 'pie'
+  },
+  title: {
+    text: undefined
+  },
+  tooltip: {
+    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+  },
+  accessibility: {
+    point: {
+      valueSuffix: '%'
+    }
+  },
+  plotOptions: {
+    pie: {
+      allowPointSelect: true,
+      cursor: 'pointer',
+      dataLabels: {
+        enabled: true,
+        format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+      }
+    }
+  },
+  series: [
+    {
+      name: 'Brands',
+      colorByPoint: true,
+      data: [
+        {
+          name: 'Chrome',
+          y: 61.41,
+          // sliced: true,
+          // selected: true
+        },
+        {
+          name: 'Internet Explorer',
+          y: 11.84
+        },
+        {
+          name: 'Firefox',
+          y: 10.85
+        },
+        {
+          name: 'Edge',
+          y: 4.67
+        },
+        {
+          name: 'Safari',
+          y: 4.18
+        },
+        {
+          name: 'Sogou Explorer',
+          y: 1.64
+        },
+        {
+          name: 'Opera',
+          y: 1.6
+        },
+        {
+          name: 'QQ',
+          y: 1.2
+        },
+        {
+          name: 'Other',
+          y: 2.61
+        }
+      ]
+    }
+  ]
+}
+
+export default {
+  title: 'Components/Chart',
+  component: EpChart,
+  decorators: [container],
+  argTypes: {
+    options: {
+      defaultValue: options,
+      table: {
+        disable: true
+      }
+    }
+  },
+  parameters: {
+    controls: { hideNoControlsWarning: true }
+  }
+}
+
+const Template = args => ({
+  components: { EpContainer, EpChart },
+  setup() {
+    return { args }
+  },
+  template: `
+    <ep-container padding="30px">
+      <ep-chart v-bind="args" />
+    </ep-container>
+  `
+})
+
+export const Chart = Template.bind({})
---
+++ src/components/charts/EpChart.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="ep-chart">
+    <div id="container"></div>
+  </div>
+</template>
+
+<script>
+  import Highcharts from 'highcharts'
+
+  export default {
+    name: 'EpChart',
+    props: {
+      options: {
+        type: Object,
+        required: true
+      }
+    },
+    data() {
+      return {
+        chart: null,
+        chartDefaults: {
+          chart:{
+            styledMode: true,
+          },
+          credits: {
+            enabled: false
+          }
+        }
+      }
+    },
+    methods: {
+      drawChart() {
+        // can dynamically change type with
+        // this.chart = Highcharts['stockChart']('container', {
+        this.chart = Highcharts.chart('container', this.chartOptions)
+      }
+    },
+    computed: {
+      chartOptions() {
+        return {
+          ...this.chartDefaults,
+          ...this.options
+        }
+      }
+    },
+    mounted() {
+      this.drawChart()
+    },
+    beforeDestroy() {
+      this.chart.destroy()
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '../../../node_modules/highcharts/css/highcharts.css';
+  // $colors: (
+  //   var(--chart-sequence-00),
+  //   var(--chart-sequence-01),
+  //   var(--chart-sequence-02),
+  //   var(--chart-sequence-03),
+  //   var(--chart-sequence-04),
+  //   var(--chart-sequence-05),
+  //   var(--chart-sequence-06),
+  //   var(--chart-sequence-07),
+  //   var(--chart-sequence-08),
+  //   var(--chart-sequence-09),
+  //   var(--chart-sequence-10),
+  //   var(--chart-sequence-11),
+  //   var(--chart-sequence-12),
+  //   var(--chart-sequence-13)
+  // );
+
+  // $font-family: 'Inter var', sans-serif;
+
+  // svg {
+  //   font-family: $font-family;
+  // }
+
+  ::v-deep {
+    .highcharts-axis-labels,
+    .highcharts-axis-title,
+    .highcharts-legend-item > text {
+      fill: var(--text-color);
+    }
+    .highcharts-container {
+      font-family: inherit;
+      font-size: var(--font-size--default);
+    }
+    .highcharts-background {
+      fill: transparent;
+    }
+    .highcharts-axis-line,
+    .highcharts-grid-line {
+      fill: none;
+      stroke: var(--border-color);
+    }
+    .highcharts-point {
+      &.highcharts-color-0 {
+        fill: var(--chart-sequence-00);
+        stroke: var(--background-2);
+      }
+      &.highcharts-color-1 {
+        fill: var(--chart-sequence-01);
+        stroke: var(--background-2);
+      }
+      &.highcharts-color-2 {
+        fill: var(--chart-sequence-02);
+        stroke: var(--background-2);
+      }
+      &.highcharts-color-3 {
+        fill: var(--chart-sequence-03);
+        stroke: var(--background-2);
+      }
+      &.highcharts-color-4 {
+        fill: var(--chart-sequence-04);
+        stroke: var(--background-2);
+      }
+      &.highcharts-color-5 {
+        fill: var(--chart-sequence-05);
+        stroke: var(--background-2);
+      }
+      &.highcharts-color-6 {
+        fill: var(--chart-sequence-06);
+        stroke: var(--background-2);
+      }
+      &.highcharts-color-7 {
+        fill: var(--chart-sequence-07);
+        stroke: var(--background-2);
+      }
+      &.highcharts-color-8 {
+        fill: var(--chart-sequence-08);
+        stroke: var(--background-2);
+      }
+      &.highcharts-color-9 {
+        fill: var(--chart-sequence-09);
+        stroke: var(--background-2);
+      }
+      &.highcharts-color-10 {
+        fill: var(--chart-sequence-10);
+        stroke: var(--background-2);
+      }
+      &.highcharts-color-11 {
+        fill: var(--chart-sequence-11);
+        stroke: var(--background-2);
+      }
+      &.highcharts-color-12 {
+        fill: var(--chart-sequence-12);
+        stroke: var(--background-2);
+      }
+      &.highcharts-color-13 {
+        fill: var(--chart-sequence-13);
+        stroke: var(--background-2);
+      }
+    }
+  }
+</style>
