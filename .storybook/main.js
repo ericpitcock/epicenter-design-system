c7019a93aca25f8a80a1018467bade4ef573853f
Eric Pitcock <accounts+git@ericpitcock.com>
2021-12-14 12:35:01 -0800

    cleaned up addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,31 +1,33 @@
 const path = require('path')
 const rootPath = path.resolve(__dirname, '../src')
 
-// import '../src/assets/scss/_mixins.scss'
-
 module.exports = {
-  stories: [
-    '../src/**/**/*.stories.js'
-  ],
-  addons: [
-    '@storybook/addon-docs',
-    '@storybook/addon-controls',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
+  stories: ['../src/**/**/*.stories.js'],
+  addons: [{
+    name: '@storybook/addon-essentials',
+    options: {
+      backgrounds: false,
+      measure: false,
+      outline: false,
+      toolbars: false,
+      viewport: false
+    }
+  },
     'storybook-theme-toggle'
   ],
   webpackFinal: async (config, { configType }) => {
     config.module.rules.push({
       test: /\.scss$/,
-      use: [ 'style-loader',
-             'css-loader',
-              {
-                loader: 'sass-loader',
-                options: {
-                  prependData: '@use "@/assets/scss/_mixins.scss";'
-                }
-              }
-            ]
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            prependData: '@use "@/assets/scss/_mixins.scss";'
+          }
+        }
+      ]
     })
     config.resolve.alias['@'] = rootPath
     config.resolve.alias['~'] = rootPath
