0be8f4cdeb2521b8e5c599fcb08a1ce6b2de098e
Eric Pitcock <accounts+git@ericpitcock.com>
2021-12-15 13:14:05 -0800

    theme switching with the store

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -16,8 +16,33 @@ export const parameters = {
   }
 }
 
-// global decorators
-// export const decorators = [(story) => ({
-//   components: { story },
-//   template: '<div style="display: grid; place-content: center; width: 100vw; height: 100vh;"><story /></div>'
-// })]
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      // Array of plain string values or MenuItem shape (see below)
+      items: ['light', 'dark'],
+      // Property that specifies if the name of the item will be displayed
+      showName: true
+    }
+  }
+}
+
+export const decorators = [
+  (story, context) => ({
+    template: '<story/>',
+    watch: {
+      theme: {
+        handler() {
+          if (context.globals.theme === store.state.theme) return
+          console.log('theme changed')
+          store.dispatch('toggleTheme')
+        },
+        immediate: true
+      }
+    }
+  })
+]
