bc44268ce44ada15098db7e309b85afa27406a88
Eric Pitcock <accounts+git@ericpitcock.com>
2023-01-06 20:51:32 -0800

    notification updates

--- src/molecules/notification/EpNotification.stories.js
+++ src/molecules/notification/EpNotification.stories.js
@@ -1,27 +1,41 @@
+import EpButton from '@/atoms/button/EpButton'
 import EpNotification from './EpNotification'
-
-// const container = () => {
-//   return {
-//     template:
-//       '<div style="padding: 30px; display:flex; justify-content: center;"><story/></div>'
-//   }
-// }
 
 export default {
   title: 'Molecules/Notification',
   component: EpNotification,
-  // decorators: [container],
   argTypes: {
+    message: {
+      name: 'Message',
+      control: {
+        type: 'text'
+      },
+      defaultValue: 'Your message was sent successfully'
+    }
   }
 }
 
-const Template = (args) => ({
-  components: { EpNotification },
+const Template = args => ({
+  components: { EpButton, EpNotification },
+  data: () => ({
+    show: false
+  }),
   setup() {
     return { args }
   },
+  methods: {
+    showNotification() {
+      this.show = true
+      setTimeout(() => {
+        this.show = false
+      }, 5000)
+    }
+  },
   template: `
-  <ep-notification />
+    <ep-button :label="'Send Message'" @click="showNotification" />
+    <Teleport to="body">
+      <ep-notification v-if="show" v-bind="args" />
+    </Teleport>
   `
 })
 
--- src/molecules/notification/EpNotification.vue
+++ src/molecules/notification/EpNotification.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="ep-notification">
-
+    <ep-container :width="'30rem'" :padding="'2rem'">
+      {{ message }}
+    </ep-container>
   </div>
 </template>
 
 <script>
+  import EpContainer from '@/atoms/container/EpContainer'
+
   export default {
     name: 'EpNotification',
     props: {
@@ -12,10 +16,10 @@
         type: String,
         required: true
       },
-      type: {
-        type: String,
-        required: true
-      },
+      // type: {
+      //   type: String,
+      //   required: true
+      // },
       dismissable: {
         type: Boolean,
         default: false
@@ -25,27 +29,32 @@
         default: 3000
       }
     },
+    components: {
+      EpContainer
+    },
     data() {
       return {
         show: false
       }
     },
-    methods: {
-      showNotification() {
-        this.show = true
-        setTimeout(() => {
-          this.show = false
-        }, this.duration)
-      }
-    },
-    mounted() {
-      this.showNotification()
-    }
+    // methods: {
+    //   showNotification() {
+    //     this.show = true
+    //     setTimeout(() => {
+    //       this.show = false
+    //     }, this.duration)
+    //   }
+    // },
+    // mounted() {
+    //   this.showNotification()
+    // }
   }
 </script>
 
 <style lang="scss" scoped>
   .ep-notification {
-
+    position: absolute;
+    top: 2rem;
+    right: 2rem;
   }
 </style>
