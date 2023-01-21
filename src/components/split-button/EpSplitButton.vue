c6f7f18183a36baa59f518dc19d006dd08042747
Eric Pitcock <accounts+git@ericpitcock.com>
2023-01-20 17:22:55 -0800

    split button init

---
+++ src/components/split-button/EpSplitButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="ep-split-button">
+    <ep-button v-bind="buttonProps" />
+    <ep-dropdown v-bind="dropdownProps" />
+  </div>
+</template>
+
+<script>
+  import EpButton from '@/components/button/EpButton'
+  import EpDropdown from '@/components/dropdown/EpDropdown'
+
+  export default {
+    name: 'EpSplitButton',
+    components: {
+      EpButton,
+      EpDropdown
+    },
+    props: {
+      buttonProps: {
+        type: Object,
+        default: () => ({})
+      },
+      dropdownProps: {
+        type: Object,
+        default: () => ({})
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .ep-split-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.1rem;
+    > .ep-button {
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+    }
+    // gotta use v-deep to override the dropdown's scoped styles
+    .ep-dropdown::v-deep .ep-button {
+      border-top-left-radius: 0;
+      border-bottom-left-radius: 0;
+    }
+  }
+</style>
