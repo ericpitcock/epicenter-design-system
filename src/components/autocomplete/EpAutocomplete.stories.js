325d8d181d9c668c87021102a847dbad8ff7b9a5
Eric Pitcock <accounts+git@ericpitcock.com>
2023-01-22 15:48:21 -0800

    autocomplete updates

--- src/components/autocomplete/EpAutocomplete.stories.js
+++ src/components/autocomplete/EpAutocomplete.stories.js
@@ -1,5 +1,11 @@
 import faker from 'faker'
+import { padded } from '@/helpers/decorators'
+import EpActionBar from '@/components/action-bar/EpActionBar'
 import EpAutocomplete from './EpAutocomplete.vue'
+import EpContainer from '@/components/container/EpContainer'
+import EpHeader from '@/components/header/EpHeader'
+import EpFooter from '@/components/footer/EpFooter'
+import E from '@/components/logo/E'
 
 const fakeArray = (length) => {
   let arr = []
@@ -15,22 +21,87 @@ const fakeArray = (length) => {
 export default {
   title: 'Components/Autocomplete',
   component: EpAutocomplete,
+  decorators: [padded],
   argTypes: {
     items: {
+      name: 'Items',
       control: {
         type: 'object'
       },
+      table: { disable: true },
       defaultValue: fakeArray(100)
+    },
+    placeholder: {
+      name: 'Placeholder',
+      control: {
+        type: 'text'
+      },
+      defaultValue: 'Search'
+    },
+    queryType: {
+      name: 'Query Type',
+      options: ['contains', 'startsWith'],
+      control: {
+        type: 'select',
+        labels: {
+          contains: 'Contains',
+          startsWith: 'Starts With'
+        }
+      },
+      defaultValue: 'contains'
     }
   }
 }
 
-const Template = (args) => ({
-  components: { EpAutocomplete },
+const Template = args => ({
+  components: {
+    EpAutocomplete,
+    EpActionBar,
+    EpContainer,
+    EpHeader,
+    EpFooter,
+    E
+  },
   setup() {
     return { args }
   },
-  template: '<EpAutocomplete v-bind="args" />'
+  template: `
+    <ep-container
+      useHeader
+      useFooter
+      height="100%"
+      padding="3rem"
+      overflow="hidden"
+    >
+      <template #header>
+      <ep-header>
+        <template #left>
+          <div style="height: 3.5rem;">
+            <e
+              faceColor="transparent"
+              highlightColor="transparent"
+              shadowColor="transparent"
+              :strokeWidth="6"
+              strokeColor="var(--sky-300)"
+            />
+          </div>
+        </template>
+        <template #right>
+          <ep-autocomplete v-bind="args" />
+        </template>
+      </ep-header>
+      </template>
+      <template #default>
+        <div class="copy-block">
+          <h1>Autocomplete</h1>
+          <p>Autocomplete is useful in user interface design because it can save users time and effort by providing suggested completions for their input, reducing the need for typing and increasing the speed of task completion. Additionally, autocomplete can help reduce errors by providing suggestions that are likely to be correct, increasing the overall usability of the interface.</p>
+        </div>
+      </template>
+      <template #footer>
+        <ep-footer />
+      </template>
+    </ep-container>
+  `
 })
 
 export const Autocomplete = Template.bind({})
--- src/components/autocomplete/EpAutocomplete.vue
+++ src/components/autocomplete/EpAutocomplete.vue
@@ -1,8 +1,8 @@
-<!-- vue 3 options api autocomplete component -->
 <template>
   <div class="ep-autocomplete">
     <input
       type="text"
+      :placeholder="placeholder"
       v-model="search"
       @input="onInput"
       @keydown.down="onKeyDown"
@@ -12,12 +12,12 @@
       @focus="onFocus"
       @blur="onBlur"
     />
-    <div class="ep-autocomplete-dropdown" v-show="showDropdown">
+    <div class="ep-autocomplete__dropdown" v-show="showDropdown">
       <div
-        class="ep-autocomplete-dropdown-item"
+        class="ep-autocomplete__dropdown__item"
         v-for="(item, index) in filteredItems"
         :key="item.id"
-        :class="{ 'ep-autocomplete-dropdown-item-active': index === currentIndex }"
+        :class="{ 'ep-autocomplete__dropdown__item--active': index === currentIndex }"
         @mouseenter="onMouseEnter(index)"
         @mousedown="onMouseDown"
       >
@@ -35,6 +35,14 @@ export default {
       type: Array,
       required: true,
     },
+    placeholder: {
+      type: String,
+      default: 'Search',
+    },
+    queryType: {
+      type: String,
+      default: 'startsWith', // contains, startsWith
+    },
   },
   data() {
     return {
@@ -45,8 +53,25 @@ export default {
   },
   computed: {
     filteredItems() {
-      return this.items.filter((item) => item.name.toLowerCase().includes(this.search.toLowerCase()));
-    },
+      // if (this.queryType === 'startsWith') {
+      //   // filter the items by those that begin with the search string
+      //   return this.items.filter((item) => {
+      //     const itemName = item.name.toLowerCase()
+      //     const search = this.search.toLowerCase()
+      //     return itemName.startsWith(search)
+      //   })
+      // } else if (this.queryType === 'contains') {
+      //   // general filter
+        
+      // }
+      return this.items.filter((item) => {
+        const itemName = item.name.toLowerCase()
+        const search = this.search.toLowerCase()
+        return (this.queryType === 'contains')
+          ? itemName.includes(search)
+          : itemName.startsWith(search)
+      })
+    }
   },
   methods: {
     onInput() {
@@ -56,10 +81,43 @@ export default {
       if (this.currentIndex < this.filteredItems.length - 1) {
         this.currentIndex += 1;
       }
+      // scroll container if needed
+      // first check if there is an active item
+      if (!document.querySelector('.ep-autocomplete-dropdown-item-active')) {
+        return;
+      }
+      const container = document.querySelector('.ep-autocomplete-dropdown');
+      const activeItem = document.querySelector('.ep-autocomplete-dropdown-item-active');
+      if (activeItem.offsetTop + activeItem.clientHeight > container.scrollTop + container.clientHeight) {
+        container.scrollTop = activeItem.offsetTop + activeItem.clientHeight - container.clientHeight;
+      }
+
+      if (activeItem.offsetTop < container.scrollTop) {
+        container.scrollTop = activeItem.offsetTop;
+      }
+
+      // scroll window if needed
+      // const rect = activeItem.getBoundingClientRect();
+      // if (rect.bottom > window.innerHeight) {
+      //   window.scrollBy(0, rect.bottom - window.innerHeight);
+      // }
+      // if (rect.top < 0) {
+      //   window.scrollBy(0, rect.top);
+      // }
     },
     onKeyUp() {
       if (this.currentIndex > 0) {
         this.currentIndex -= 1;
+      }
+      // keep the active item in the middle of the container
+      const container = document.querySelector('.ep-autocomplete-dropdown');
+      const activeItem = document.querySelector('.ep-autocomplete-dropdown-item-active');
+      if (activeItem.offsetTop + activeItem.clientHeight > container.scrollTop + container.clientHeight) {
+        container.scrollTop = activeItem.offsetTop + activeItem.clientHeight - container.clientHeight;
+      }
+
+      if (activeItem.offsetTop < container.scrollTop) {
+        container.scrollTop = activeItem.offsetTop;
       }
     },
     onEnter() {
@@ -70,7 +128,7 @@ export default {
       this.showDropdown = false;
     },
     onFocus() {
-      this.showDropdown = true;
+      // this.showDropdown = true;
     },
     onBlur() {
       this.showDropdown = false;
@@ -81,9 +139,9 @@ export default {
     onMouseDown() {
       this.search = this.filteredItems[this.currentIndex].name;
       this.showDropdown = false;
-    },
-  },
-};
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -94,26 +152,26 @@ export default {
   input {
     width: 100%;
     padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
+    border: 1px solid var(--border-color);
+    border-radius: var(--border-radius);
     outline: none;
   }
-  .ep-autocomplete-dropdown {
+  &__dropdown {
     position: absolute;
-    top: 100%;
+    top: calc(100% + 0.4rem);
     left: 0;
     width: 100%;
     max-height: 200px;
     overflow-y: auto;
-    background-color: var(--background-2);
-    border: 1px solid #ccc;
-    border-radius: 4px;
+    background-color: var(--background-4);
+    border: 1px solid var(--border-color--lighter);
+    border-radius: var(--border-radius);
     z-index: 1;
-    .ep-autocomplete-dropdown-item {
+    &__item {
       padding: 10px;
       cursor: pointer;
-      &-active {
-        background-color: #eee;
+      &--active {
+        background-color: var(--color--primary);
       }
     }
   }
