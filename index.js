a242f29da1e854b7038718c321d748253849766e
Eric Pitcock <accounts+git@ericpitcock.com>
2023-02-14 20:25:24 -0800

    import stuff

--- index.js
+++ index.js
@@ -1,42 +1,34 @@
-/* To dynamically import all components with a .vue extension from the components directory,
-you can use the require.context function in Webpack. Here's an example of how to use it:
-*/
+import * as components from './src/components'
 
-const files = require.context('./src/components', true, /\.vue$/i)
-const components = {}
-
-files.keys().forEach(key => {
-  const name = key.match(/([^/]+)\.vue$/i)[1]
-  components[name] = files(key).default || files(key)
+// add install method to components
+Object.keys(components).forEach(key => {
+  components[key].install = function (Vue) {
+    Vue.component(components[key].name, components[key])
+  }
 })
 
-export default components
+// export components
+// export default components
 
-/*
-In this example, we're using require.context to get a list of all files in the ./components directory that end with .vue.
-We then loop through the list of files, extract the component name from the file path, and add the component to the components object.
-Note that the default property is used here in case the component is exported as the default export (e.g., export default { ...}).
-If the component is not exported as the default export, we use the module itself (i.e., files(key)).
-Once you have this components object, you can use it to register your components globally or locally in your Vue app.
-Here's an example of how to register your components globally:
-*/
+// export install method
+export const install = function (Vue) {
+  Object.keys(components).forEach(key => {
+    Vue.component(components[key].name, components[key])
+  })
+}
 
-// import Vue from 'vue'
-// import components from './components'
+export * from './src/components'
 
-// Object.keys(components).forEach(name => {
-//   Vue.component(name, components[name])
-// })
+// // import components
+// import { Button, Select } from 'my-ui'
 
-// This will register all of your components globally so they can be used in any Vue component in your app.
-// Alternatively, you can register your components locally in a specific Vue component:
+// // install components
+// Vue.use(Button)
 
-// import components from './components'
+// // use components
+// <Button>Button</Button>
 
-// export default {
-//   components,
-//   // ...
-// }
-
-// try to use the components in the template of the component:
-// import { Button, Input, Select } from '@ericpitcock/vue-components';
+// // use components
+// <Select>
+//   <Option value="1">Option 1</Option>
+// </Select>
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import EpActionBar from './action-bar/EpActionBar.vue'
+import EpButton from './button/EpButton.vue'
+
+export {
+  EpActionBar,
+  EpButton
+}
