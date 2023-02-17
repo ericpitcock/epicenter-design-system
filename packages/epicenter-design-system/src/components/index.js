fe61b38960fe4f7ad7e629d49b1dca24bf299ae5
Eric Pitcock <accounts+git@ericpitcock.com>
2023-02-16 18:26:36 -0800

    stuff

--- packages/epicenter-design-system/src/components/index.js
+++ packages/epicenter-design-system/src/components/index.js
@@ -1,57 +1,57 @@
-import EpActionBar from './action-bar/EpActionBar.vue'
-import EpAutocomplete from './autocomplete/EpAutocomplete.vue'
-import EpBadge from './badge/EpBadge.vue'
-import EpBanner from './banner/EpBanner.vue'
-import EpBreadcrumb from './breadcrumb/EpBreadcrumb.vue'
+// import EpActionBar from './action-bar/EpActionBar.vue'
+// import EpAutocomplete from './autocomplete/EpAutocomplete.vue'
+// import EpBadge from './badge/EpBadge.vue'
+// import EpBanner from './banner/EpBanner.vue'
+// import EpBreadcrumb from './breadcrumb/EpBreadcrumb.vue'
 import EpButton from './button/EpButton.vue'
-import EpChart from './charts/EpChart.vue'
-import EpCheckbox from './checkbox/EpCheckbox.vue'
-import EpContainer from './container/EpContainer.vue'
-import EpDatePicker from './date-picker/EpDatePicker.vue'
-import EpDivider from './divider/EpDivider.vue'
-import EpDropdown from './dropdown/EpDropdown.vue'
-import EpEmptyState from './empty-state/EpEmptyState.vue'
-import EpFlexContainer from './flexbox/EpFlexContainer.vue'
-import EpFooter from './footer/EpFooter.vue'
-import EpHeader from './header/EpHeader.vue'
+// import EpChart from './charts/EpChart.vue'
+// import EpCheckbox from './checkbox/EpCheckbox.vue'
+// import EpContainer from './container/EpContainer.vue'
+// import EpDatePicker from './date-picker/EpDatePicker.vue'
+// import EpDivider from './divider/EpDivider.vue'
+// import EpDropdown from './dropdown/EpDropdown.vue'
+// import EpEmptyState from './empty-state/EpEmptyState.vue'
+// import EpFlexContainer from './flexbox/EpFlexContainer.vue'
+// import EpFooter from './footer/EpFooter.vue'
+// import EpHeader from './header/EpHeader.vue'
 import EpIcon from './icon/EpIcon.vue'
-import EpBasicInput from './input/EpBasicInput.vue'
-import EpLoading from './loading/EpLoading.vue'
-import EpMap from './map/EpMap.vue'
-import EpMenu from './menu/EpMenu.vue'
-import EpNotification from './notification/EpNotification.vue'
-import EpSelect from './select/EpSelect.vue'
-import EpSparkBar from './spark-bar/EpSparkBar.vue'
-import EpSplitButton from './split-button/EpSplitButton.vue'
-import EpTable from './table/EpTable.vue'
-import EpTabs from './tabs/EpTabs.vue'
+// import EpBasicInput from './input/EpBasicInput.vue'
+// import EpLoading from './loading/EpLoading.vue'
+// import EpMap from './map/EpMap.vue'
+// import EpMenu from './menu/EpMenu.vue'
+// import EpNotification from './notification/EpNotification.vue'
+// import EpSelect from './select/EpSelect.vue'
+// import EpSparkBar from './spark-bar/EpSparkBar.vue'
+// import EpSplitButton from './split-button/EpSplitButton.vue'
+// import EpTable from './table/EpTable.vue'
+// import EpTabs from './tabs/EpTabs.vue'
 
-export {
-  EpActionBar,
-  EpAutocomplete,
-  EpBadge,
-  EpBanner,
-  EpBreadcrumb,
+export default {
+  // EpActionBar,
+  // EpAutocomplete,
+  // EpBadge,
+  // EpBanner,
+  // EpBreadcrumb,
   EpButton,
-  EpChart,
-  EpCheckbox,
-  EpContainer,
-  EpDatePicker,
-  EpDivider,
-  EpDropdown,
-  EpEmptyState,
-  EpFlexContainer,
-  EpFooter,
-  EpHeader,
+  // EpChart,
+  // EpCheckbox,
+  // EpContainer,
+  // EpDatePicker,
+  // EpDivider,
+  // EpDropdown,
+  // EpEmptyState,
+  // EpFlexContainer,
+  // EpFooter,
+  // EpHeader,
   EpIcon,
-  EpBasicInput,
-  EpLoading,
-  EpMap,
-  EpMenu,
-  EpNotification,
-  EpSelect,
-  EpSparkBar,
-  EpSplitButton,
-  EpTable,
-  EpTabs
+  // EpBasicInput,
+  // EpLoading,
+  // EpMap,
+  // EpMenu,
+  // EpNotification,
+  // EpSelect,
+  // EpSparkBar,
+  // EpSplitButton,
+  // EpTable,
+  // EpTabs
 }
