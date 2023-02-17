2803295f514cd04ac552bc53f9851e1ec0409464
Eric Pitcock <accounts+git@ericpitcock.com>
2023-02-17 13:16:27 -0800

    storybook main.js cleanup

--- .storybook/main.js
+++ .storybook/main.js
@@ -24,25 +24,14 @@ module.exports = {
         'css-loader',
         {
           loader: 'sass-loader',
-          // options: {
-          //   prependData: '@import "../../packages/epicenter-design-system/src/assets/scss/_mixins.scss";'
-          // }
+          options: {
+            prependData: '@import "@/assets/scss/_mixins.scss";'
+          }
         }
       ]
     })
     config.resolve.alias['@'] = rootPath
     config.resolve.alias['~'] = rootPath
-
-    // // Fix wrong project root on server
-    // // Storybook treats the nearest ".git" folder as its project root. However, on a server that splits the Git repository from the working directory, this leads to a wrong project root.
-    // // https://github.com/storybookjs/storybook/blob/next/lib/core-common/src/utils/paths.ts
-
-    // const babelLoaderRule = config.module.rules.find(
-    //   // https://github.com/storybookjs/storybook/blob/next/lib/builder-webpack4/src/preview/babel-loader-preview.ts
-    //   (rule) => rule.test.toString() === /\.(mjs|tsx?|jsx?)$/.toString()
-    // )
-    // // set correct project root
-    // babelLoaderRule.include = [path.resolve(__dirname, "../..")]
 
     return config
   }
