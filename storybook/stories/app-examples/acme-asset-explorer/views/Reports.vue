b12efc542e94cd48bcdbca01703d5e9f01f07d28
Eric Pitcock <accounts+git@ericpitcock.com>
2023-02-26 16:04:25 -0800

    removed acme asset explorer in favor of real site

--- storybook/stories/app-examples/acme-asset-explorer/views/Reports.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <p style="padding: 3rem">This view is in dev. But check out <router-link style="text-decoration: underline; cursor: pointer;" to="/assets">Assets</router-link> for something more complete.</p>
-</template>
