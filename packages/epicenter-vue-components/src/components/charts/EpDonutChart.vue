7b452a415d2cebbff1a8092390f035af30896eb8
Eric Pitcock <accounts+git@ericpitcock.com>
2023-03-07 23:19:17 -0800

    donut chart tweaks

--- packages/epicenter-vue-components/src/components/charts/EpDonutChart.vue
+++ packages/epicenter-vue-components/src/components/charts/EpDonutChart.vue
@@ -21,7 +21,7 @@
       drawChart() {
         // Set up the data
         var data = [20, 30, 40, 50]
-        var labels = ["Label 1", "Label 2", "Label 3", "Label 4"]
+        var labels = ['Label 1', 'Label 2', 'Label 3', 'Label 4']
         var total = d3.sum(data)
 
         // Set up the dimensions and margins of the chart
@@ -33,22 +33,22 @@
         var radius = Math.min(width, height) / 2 - margin
 
         // Select the SVG element and set its dimensions
-        var svg = d3.select("#chart")
-          .append("svg")
-          .attr("width", width)
-          .attr("height", height)
+        var svg = d3.select('#chart')
+          .append('svg')
+          .attr('width', width)
+          .attr('height', height)
 
         // Create a group element for the chart
-        var g = svg.append("g")
-          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+        var g = svg.append('g')
+          .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
 
         // Set up the color scale
         var color = d3.scaleOrdinal()
-          .range(["#FF6384", "#36A2EB", "#FFCE56", "#33FF99"])
+          .range(['var(--chart-sequence-1)', 'var(--chart-sequence-2)', 'var(--chart-sequence-3)', 'var(--chart-sequence-4)'])
 
         // Set up the arc generator
         var arc = d3.arc()
-          .innerRadius(radius - 20)
+          .innerRadius(radius - 26)
           .outerRadius(radius)
 
         // Set up the pie generator
@@ -57,32 +57,58 @@
           .value(function (d) { return d })
 
         // Generate the arcs
-        var arcs = g.selectAll("arc")
+        var arcs = g.selectAll('arc')
           .data(pie(data))
           .enter()
-          .append("g")
-          .attr("class", "arc")
+          .append('g')
+          .attr('class', 'arc')
 
         // Draw the paths for the arcs
-        arcs.append("path")
-          .attr("d", arc)
-          .attr("fill", function (d) { return color(d.data) })
+        arcs.append('path')
+          .attr('d', arc)
+          .attr('fill', function (d) { return color(d.data) })
+          .attr('stroke', 'var(--background-1)')
+          .attr('stroke-width', '0.3rem')
+        // animate the arcs
+        // .transition()
+        // .duration(2000) // Set the duration of the animation to 2 seconds
+        // .attrTween('d', function (d) {
+        //   var interpolate = d3.interpolate(d.startAngle, d.endAngle)
+        //   return function (t) {
+        //     d.endAngle = interpolate(t)
+        //     return arc(d)
+        //   }
+        // })
 
         // Add the labels to the arcs
-        arcs.append("text")
-          .attr("transform", function (d) { return "translate(" + arc.centroid(d) + ")" })
-          .attr("text-anchor", "middle")
-          .text(function (d, i) { return labels[i] })
+        // arcs.append('text')
+        //   .attr('transform', function (d) { return 'translate(' + arc.centroid(d) + ')' })
+        //   .attr('text-anchor', 'middle')
+        //   .text(function (d, i) { return labels[i] })
 
         // Add the total value to the center of the chart
-        g.append("text")
-          .attr("text-anchor", "middle")
+        g.append('text')
+          .attr('text-anchor', 'middle')
           .text(total)
-
+          .attr('fill', 'var(--text-color)')
+          .attr('font-size', '3rem')
+          .attr('font-family', 'Inter var, sans-serif')
+          .attr('font-weight', '300')
+          // move text down a bit
+          .attr('dy', '0.3em')
+        // css class
+        // .attr('class', '.font-size--large')
+        // animate the total
+        // .text(0)
+        // .transition()
+        // .duration(2000) // Set the duration of the animation to 2 seconds
+        // .tween('text', function (d) {
+        //   var i = d3.interpolate(0, total)
+        //   return function (t) {
+        //     d3.select(this).text(Math.round(i(t)))
+        //   }
+        // })
       }
     }
   }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
