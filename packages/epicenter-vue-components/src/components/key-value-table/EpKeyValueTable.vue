6030d3180f8bc31487e6ffcde9e2810b80ef853c
Eric Pitcock <accounts+git@ericpitcock.com>
2023-04-22 14:28:23 -0700

    key value table component

---
+++ packages/epicenter-vue-components/src/components/key-value-table/EpKeyValueTable.vue
@@ -0,0 +1,117 @@
+<template>
+  <template
+    v-for="(rows, section) in data"
+    :key="section"
+  >
+    <h3 v-if="sectionHeaders">
+      {{ section }}
+    </h3>
+    <table
+      :class="['ep-key-value-table', classes]"
+      :style="{ width: width }"
+    >
+      <tr
+        v-for="(row, key) in rows"
+        :key="key"
+      >
+        <td
+          class="text--subtle text-align--right"
+          :style="{ width: keyColumnWidth }"
+        >
+          {{ camelCaseToTitleCase(key) }}
+        </td>
+        <td>{{ row }}</td>
+      </tr>
+    </table>
+  </template>
+</template>
+
+<script>
+  export default {
+    name: 'EpKeyValueTable',
+    props: {
+      data: {
+        type: Object,
+        required: true
+      },
+      width: {
+        type: String,
+        default: '100%'
+      },
+      commonKeyWidth: {
+        type: Boolean,
+        default: false
+      },
+      acronyms: {
+        type: Array,
+        default: () => []
+      },
+      sectionHeaders: {
+        type: Boolean,
+        default: false
+      },
+      striped: {
+        type: Boolean,
+        default: false
+      }
+    },
+    data() {
+      return {
+        showSecondary: true
+      }
+    },
+    computed: {
+      classes() {
+        return {
+          'ep-key-value-table--striped': this.striped
+        }
+      },
+      keyColumnWidth() {
+        if (!this.commonKeyWidth) {
+          return 'auto'
+        }
+
+        const data = this.data
+        let maxKeyLength = 0
+
+        for (let key of Object.keys(data)) {
+          for (let subKey in data[key]) {
+            if (subKey.length > maxKeyLength) {
+              maxKeyLength = subKey.length
+            }
+          }
+        }
+
+        return `${maxKeyLength}ch`
+      }
+    },
+    methods: {
+      camelCaseToTitleCase(str) {
+        return str
+          .replace(/([A-Z])/g, ' $1')
+          .replace(/^./, function (str) {
+            return str.toUpperCase()
+          })
+          // handle acronyms
+          .replace(/(Id|Ip|Http|Xff|Amp|Dns|Mac|Os|Url)/g, function (str) {
+            return str.toUpperCase()
+          })
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .ep-key-value-table {
+    td {
+      padding: 0.5rem 1rem;
+      border-bottom: 1px solid var(--border-color);
+    }
+
+    &--striped {
+      tr:nth-child(even) {
+        background-color: var(--table-stripe-color);
+      }
+    }
+  }
+</style>
---
+++ storybook/stories/components/EpKeyValueTable.stories.js
@@ -0,0 +1,111 @@
+import { padded } from '../../helpers/decorators'
+import EpContainer from '@/components/container/EpContainer.vue'
+import EpKeyValueTable from '@/components/key-value-table/EpKeyValueTable.vue'
+
+const data = {
+  primary: {
+    applicationProtocol: 'https',
+    port: 49582,
+    timestamp: '2023-04-20T07:43:47.170Z',
+    transportProtocol: 'TCP',
+    ipAddress: '175.130.222.219',
+    transportHostType: 'Virtual',
+    applicationHostType: 'Virtual',
+    connectionDisrupted: false,
+    customerAsset: 'No',
+    xffIpAddress: '43.101.194.54',
+    xffAmpStatus: 'Enabled',
+    dnsType: 'CNAME',
+    dnsResourceName: 'dark-terrace.org',
+    dnsResourceType: 'A',
+    dnsResourceData: '164.221.252.124',
+    city: 'Tremblayborough'
+  },
+  secondary: {
+    httpHostname: 'juicy-jewel.name',
+    httpUrl: 'https://strict-tabby.name',
+    httpContentType: 'application/tamp-apex-update',
+    httpUserAgent: 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0; .NET CLR 1.4.41485.2)',
+    httpReferrer: 'http://upbeat-face.org',
+    httpMethod: 'DELETE',
+    hostname: 'live-lens.name',
+    domain: 'finished-passion.name',
+    macAddress: '43:6e:97:4d:87:e1',
+    country: 'Guam',
+    company: 'Jones Inc',
+    ampStatus: 'Enabled',
+    ampComment: 'Placeat blanditiis perferendis enim.',
+    xffHostname: 'worthwhile-wasabi.com',
+    xffDomain: 'brown-squash.biz',
+    xffCountry: 'Costa Rica',
+    xffRegion: 'Cambridgeshire',
+    xffCity: 'Jerrodbury',
+    xffCompany: 'Langosh, Heidenreich and Fadel',
+    xffAmpComment: 'Occaecati in ratione modi perferendis accusantium porro suscipit quo doloremque.'
+  }
+}
+
+export default {
+  title: 'Components/Table/Key Value Table',
+  component: EpKeyValueTable,
+  decorators: [padded],
+  argTypes: {
+    data: {
+      name: 'Data',
+      control: {
+        type: 'object'
+      }
+    },
+    width: {
+      name: 'Width',
+      control: {
+        type: 'text'
+      }
+    },
+    commonKeyWidth: {
+      name: 'Common Key Width',
+      control: {
+        type: 'boolean'
+      }
+    },
+    acronyms: {
+      name: 'Acronyms',
+      control: {
+        type: 'array'
+      }
+    },
+    sectionHeaders: {
+      name: 'Section Headers',
+      control: {
+        type: 'boolean'
+      }
+    },
+    striped: {
+      name: 'Striped',
+      control: {
+        type: 'boolean'
+      }
+    },
+  }
+}
+
+export const KeyValueTable = (args) => ({
+  components: { EpContainer, EpKeyValueTable },
+  setup () {
+    return { args }
+  },
+  template: `
+  <ep-container content-padding="3rem">
+    <ep-key-value-table v-bind="args" />
+  </ep-container>
+  `
+})
+
+KeyValueTable.args = {
+  data: data,
+  width: '100%',
+  commonKeyWidth: true,
+  acronyms: ['IP', 'DNS', 'HTTP', 'XFF'],
+  sectionHeaders: true,
+  striped: true,
+}
