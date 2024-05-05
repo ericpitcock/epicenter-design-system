45d0bfba656b8fa662c57d910fd6d888fc1110e0
Eric Pitcock <accounts+git@ericpitcock.com>
2024-05-05 11:20:56 -0700

    cleanup

--- new-table/EpTable.vue
+++ new-table/EpTable.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
-    <!-- Search input -->
-    <input
-      v-model="searchText"
+    <ep-table-search-input
       v-if="enableSearch"
-      placeholder="Search..."
-    >
+      :search-text="searchText"
+      @update:search-text="updateSearchText"
+    />
     <table>
       <thead>
         <tr>
@@ -14,7 +13,6 @@
             :key="column.key"
           >
             {{ column.label }}
-            <!-- Sorting indicator -->
             <span
               v-if="column.sortable && enableSorting"
               @click="sortBy(column.key)"
@@ -37,30 +35,40 @@
         </tr>
       </tbody>
     </table>
-    <!-- Pagination controls -->
     <div v-if="enablePagination">
       <button
-        @click="currentPage--"
         :disabled="currentPage === 1"
-      >Previous</button>
+        @click="currentPage--"
+      >
+        Previous
+      </button>
       <span>{{ currentPage }}</span>
       <button
-        @click="currentPage++"
         :disabled="currentPage === totalPages"
-      >Next</button>
+        @click="currentPage++"
+      >
+        Next
+      </button>
     </div>
   </div>
 </template>
 
 <script setup>
   import { ref, defineProps } from 'vue'
+  import EpTableSearchInput from './EpTableSearchInput.vue'
   import useSorting from './useSorting'
   import usePagination from './usePagination'
   import useSearch from './useSearch'
 
   const props = defineProps({
-    columns: Array,
-    data: Array,
+    columns: {
+      type: Array,
+      required: true
+    },
+    data: {
+      type: Array,
+      required: true
+    },
     pageSize: {
       type: Number,
       default: 10
@@ -79,15 +87,24 @@
     }
   })
 
-  const { sortedData, sortBy, sortColumn, sortOrder } = useSorting(props.data, props.enableSorting)
+  // Your data
+  const yourData = ref([])
+
+  const searchText = ref('')
+  const { sortedData, sortBy, sortColumn, sortOrder } = useSorting(yourData, props.enableSorting)
 
   const { paginatedData, currentPage } = usePagination(sortedData, props.pageSize, props.enablePagination)
   const totalPages = Math.ceil(sortedData.length / props.pageSize)
 
-  const { searchText, searchedData } = useSearch(paginatedData, props.enableSearch)
+  const { searchedData } = useSearch(paginatedData, props.enableSearch, searchText)
 
   // Combine pagination and search
   const paginatedAndSearchedData = computed(() => {
     return searchedData.value.slice((currentPage - 1) * props.pageSize, currentPage * props.pageSize)
   })
+
+  // Method to update search text
+  const updateSearchText = (text) => {
+    searchText.value = text
+  }
 </script>
