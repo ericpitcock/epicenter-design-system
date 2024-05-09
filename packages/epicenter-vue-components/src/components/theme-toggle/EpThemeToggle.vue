1dc577b5b0cf1aef7ce1bdf340984dc7d84241eb
Eric Pitcock <accounts+git@ericpitcock.com>
2024-05-09 09:20:53 -0700

    theme toggle to script setup

--- packages/epicenter-vue-components/src/components/theme-toggle/EpThemeToggle.vue
+++ packages/epicenter-vue-components/src/components/theme-toggle/EpThemeToggle.vue
@@ -1,36 +1,27 @@
 <template>
-  <ep-button
-    variant="ghost"
-    :icon-left="{ name: buttonIcon, color: iconColor }"
+  <EpButton
+    :icon-left="{ name: buttonIcon }"
     title="Toggle theme"
-    class="theme-toggle"
     @click="toggleTheme"
   />
 </template>
 
-<script>
-  // import { mapActions, mapState } from 'vuex'
+<script setup>
   import EpButton from '../button/EpButton.vue'
+  import { computed } from 'vue'
 
-  export default {
-    name: 'EpThemeToggle',
-    components: {
-      EpButton
-    },
-    props: {
-      iconColor: {
-        type: String,
-        default: 'currentColor'
-      }
-    },
-    computed: {
-      // ...mapState(['theme']),
-      buttonIcon() {
-        return this.theme == 'dark' ? 'light-mode' : 'dark-mode'
-      }
-    },
-    methods: {
-      // ...mapActions(['toggleTheme'])
+  const props = defineProps({
+    currentTheme: {
+      type: String,
+      required: true
     }
+  })
+
+  const buttonIcon = computed(() => props.currentTheme === 'dark' ? 'light-mode' : 'dark-mode')
+
+  const emit = defineEmits(['toggle-theme'])
+
+  const toggleTheme = () => {
+    emit('toggle-theme')
   }
 </script>
--- storybook/stories/components/EpThemeToggle.stories.js
+++ storybook/stories/components/EpThemeToggle.stories.js
@@ -1,5 +1,7 @@
 import { centeredSurface } from '../../helpers/decorators.js'
 import EpThemeToggle from '@/components/theme-toggle/EpThemeToggle.vue'
+import { useStorybookStore } from '../../store'
+import { computed } from 'vue'
 
 export default {
   title: 'Components/Theme Toggle',
@@ -12,5 +14,20 @@ export default {
 
 export const ThemeToggle = () => ({
   components: { EpThemeToggle },
-  template: '<ep-theme-toggle />'
+  setup() {
+
+    const store = useStorybookStore()
+    const currentTheme = computed(() => store.theme)
+
+    const onThemeChange = () => {
+      store.toggleTheme()
+    }
+    return { currentTheme, onThemeChange }
+  },
+  template: `
+    <ep-theme-toggle
+      :current-theme="currentTheme"
+      @toggle-theme="onThemeChange"
+    />
+  `
 })
