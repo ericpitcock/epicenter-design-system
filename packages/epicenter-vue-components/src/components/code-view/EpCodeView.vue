d54ce77b10b3487a4d10df750ef3400378fab5e8
Eric Pitcock <accounts+git@ericpitcock.com>
2024-05-08 17:54:24 -0700

    code view to script setup

--- packages/epicenter-vue-components/src/components/code-view/EpCodeView.vue
+++ packages/epicenter-vue-components/src/components/code-view/EpCodeView.vue
@@ -2,47 +2,35 @@
   <pre><code :class="classes">{{ code }}</code></pre>
 </template>
 
-<script>
+<script setup>
+  import { computed, onMounted } from 'vue'
   import Prism from 'prismjs'
   import './prism-onedark.css'
   import 'prismjs/components/prism-javascript'
   import 'prismjs/components/prism-json'
   import 'prismjs/plugins/line-numbers/prism-line-numbers.min.js'
 
-  export default {
-    name: 'EpCodeView',
-    props: {
-      code: {
-        type: String,
-        required: true
-      },
-      language: {
-        type: String,
-        required: true
-      },
-      lineNumbers: {
-        type: Boolean,
-        default: false
-      }
+  const props = defineProps({
+    code: {
+      type: String,
+      required: true
     },
-    computed: {
-      classes() {
-        return [
-          `language-${this.language}`,
-          { 'line-numbers': this.lineNumbers }
-        ]
-      }
+    language: {
+      type: String,
+      required: true
     },
-    watch: {
-      code() {
-        // using nextTick to ensure the DOM is updated before highlighting
-        this.$nextTick(() => {
-          Prism.highlightAll()
-        })
-      },
-    },
-    mounted() {
-      Prism.highlightAll()
+    lineNumbers: {
+      type: Boolean,
+      default: true
     }
-  }
+  })
+
+  const classes = computed(() => [
+    `language-${props.language}`,
+    { 'line-numbers': props.lineNumbers }
+  ])
+
+  onMounted(() => {
+    Prism.highlightAll()
+  })
 </script>
