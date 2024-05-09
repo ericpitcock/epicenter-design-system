0d6bfd9026017acf75c70fc80cfcfba507816093
Eric Pitcock <accounts+git@ericpitcock.com>
2024-05-08 19:41:27 -0700

    JSON format

--- storybook/helpers/codeExamples.js
+++ storybook/helpers/codeExamples.js
@@ -64,9 +64,18 @@ export const codeExamples = {
   "name": "John",
   "age": 25,
   "cars": [
-    { "name": "Ford", "models": ["Fiesta", "Focus", "Mustang"] },
-    { "name": "BMW", "models": ["320", "X3", "X5"] },
-    { "name": "Fiat", "models": ["500", "Panda"] }
+    { 
+      "name": "Ford",
+      "models": ["Fiesta", "Focus", "Mustang"]
+    },
+    {
+      "name": "BMW",
+      "models": ["320", "X3", "X5"]
+    },
+    {
+      "name": "Fiat",
+      "models": ["500", "Panda"]
+    }
   ]
 }`,
 }
