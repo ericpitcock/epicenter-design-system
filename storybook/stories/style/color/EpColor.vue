eff8275588ef6c1899b405872df0538a5770600c
Eric Pitcock <accounts+git@ericpitcock.com>
2024-05-11 11:52:33 -0700

    added chroma.js, color cleanup

--- storybook/stories/style/color/EpColor.vue
+++ storybook/stories/style/color/EpColor.vue
@@ -11,7 +11,6 @@
       id="colors__table"
       class="colors__table"
       width="fit-content"
-      container-padding="0 3rem"
       style="overscroll-behavior: contain"
       calculate-height
       :calculate-height-offset="30"
@@ -26,7 +25,7 @@
         sticky-header
         calculate-height
         :calculate-height-offset="30"
-        padding="0 0 10rem 0"
+        padding="0 3rem 10rem 3rem"
       />
     </ep-container>
   </div>
@@ -39,6 +38,7 @@
   import colors from './color.json'
   import grayscale from './grayscale.json'
   import copyToClipboard from '@/mixins/copyToClipboard.js'
+  import chroma from 'chroma-js'
 
   export default {
     name: 'EpColor',
@@ -196,9 +196,6 @@
       backgroundColor() {
         return this.theme === 'dark' ? '#1f1f1f' : '#ebebeb'
       },
-      // currentTheme() {
-      //   return this.theme === 'Dark Theme' ? 'dark' : 'light'
-      // },
       filteredData() {
         if (this.filter === '') {
           return this.tableData
@@ -249,33 +246,8 @@
         }
         return data
       }
-      // chartSequence() {
-      //   let data = []
-      //   const htmlStyles = window.getComputedStyle(document.querySelector('html'))
-      //   for (let index = 0; index < 14; index++) {
-      //     const cssVar = `--chart-sequence-${index}`
-      //     const hexValue = htmlStyles.getPropertyValue('--chart-sequence-' + index)
-      //     data.push({
-      //       sample: hexValue,
-      //       color: `Chart Sequence ${index}`,
-      //       css: cssVar,
-      //       hex: hexValue
-      //     })
-      //   }
-      //   return data
-      // }
     },
     watch: {
-      // watch theme from mapState
-      // $theme() {
-      //   this.tableData()
-      //   console.log('theme changed')
-      // },
-
-      // theme() {
-      //   this.tableData()
-      //   console.log('theme changed')
-      // },
       filteredData() {
         // scroll color table to the top when filter changes
         const colorTableContainer = document.querySelector('.ep-table-container')
@@ -284,58 +256,17 @@
     },
     methods: {
       contrast(color) {
-        // const background = window.getComputedStyle(document.querySelector('html')).getPropertyValue('--background-1')
         const background = this.backgroundColor
-        var rgb = function(color) {
-          if (color.indexOf('rgb') === 0) {
-            var arr = color.match(/\d+/g)
-            return {
-              r: arr[0],
-              g: arr[1],
-              b: arr[2]
-            }
-          } else if (color.indexOf('#') === 0) {
-            var hex = color.length === 4 ? color.replace(/#([0-9a-f])([0-9a-f])([0-9a-f])/i, '#$1$1$2$2$3$3') : color
-            return {
-              r: parseInt(hex.substr(1, 2), 16),
-              g: parseInt(hex.substr(3, 2), 16),
-              b: parseInt(hex.substr(5, 2), 16)
-            }
-          } else {
-            return {
-              r: 0,
-              g: 0,
-              b: 0
-            }
-          }
-        }
-        var luminance = function(color) {
-          var c = rgb(color)
-          var r = c.r / 255
-          var g = c.g / 255
-          var b = c.b / 255
-          var R = r <= 0.03928 ? r / 12.92 : Math.pow((r + 0.055) / 1.055, 2.4)
-          var G = g <= 0.03928 ? g / 12.92 : Math.pow((g + 0.055) / 1.055, 2.4)
-          var B = b <= 0.03928 ? b / 12.92 : Math.pow((b + 0.055) / 1.055, 2.4)
-          return 0.2126 * R + 0.7152 * G + 0.0722 * B
-        }
-        var contrast = function(color, background) {
-          var l1 = luminance(color)
-          var l2 = luminance(background)
-          return l1 > l2 ? (l1 + 0.05) / (l2 + 0.05) : (l2 + 0.05) / (l1 + 0.05)
-        }
-        var c = contrast(color, background)
-        if (c < 4.5) {
+        const contrast = chroma.contrast(color, background)
+
+        if (contrast < 4.5) {
           return '&#x26A0;'
-        }
-        // if c is greater than or equal to 4.5 but less than 7, return 'AA'
-        else if (c >= 4.5 && c < 7) {
+        } else if (contrast >= 4.5 && contrast < 7) {
           return 'AA &check;'
-        } else if (c >= 7) {
+        } else if (contrast >= 7) {
           return 'AAA &check;'
         }
-        // return false;
-      }
+      },
     },
   }
 </script>
