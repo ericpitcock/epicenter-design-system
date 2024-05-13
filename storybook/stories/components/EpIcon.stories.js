a7e0c8f7d5fe72e3dfbd9bb198de1ae26475111f
Eric Pitcock <accounts+git@ericpitcock.com>
2024-05-13 15:18:16 -0700

    icon prog

--- storybook/stories/components/EpIcon.stories.js
+++ storybook/stories/components/EpIcon.stories.js
@@ -2,54 +2,69 @@ import EpContainer from '@/components/container/EpContainer.vue'
 import EpIcon from '@/components/icon/EpIcon.vue'
 import { iconNamesObject } from '../../helpers/iconHelper.js'
 import { centeredSurface } from '../../helpers/decorators.js'
-import { iconArgTypes } from '../../helpers/iconArgTypes.js'
+import { computed } from 'vue'
+import { iconOptions } from '../../helpers/iconHelper.js'
 
 export default {
   title: 'Components/Icon',
   component: EpIcon,
-  argTypes: iconArgTypes
-  // argTypes: {
-  //   name: {
-  //     name: 'Name',
-  //     options: iconOptions,
-  //     control: {
-  //       type: 'select'
-  //     }
-  //   },
-  //   color: {
-  //     name: 'Color',
-  //     control: {
-  //       type: 'color'
-  //     }
-  //   },
-  //   weight: {
-  //     name: 'Weight',
-  //     options: ['extra-light', 'light', 'regular', 'bold'],
-  //     control: {
-  //       type: 'radio'
-  //     }
-  //   },
-  //   size: {
-  //     name: 'Size',
-  //     control: {
-  //       type: 'range',
-  //       min: 20,
-  //       max: 60,
-  //       step: 2
-  //     }
-  //   },
-  //   type: {
-  //     table: { disable: true }
-  //   }
-  // }
+  argTypes: {
+    name: {
+      name: 'Name',
+      options: iconOptions,
+      control: {
+        type: 'select'
+      }
+    },
+    color: {
+      name: 'Color',
+      control: {
+        type: 'color'
+      }
+    },
+    weight: {
+      name: 'Weight',
+      options: ['extra-light', 'light', 'regular', 'bold'],
+      mapping: {
+        'extra-light': 0.5,
+        'light': 1,
+        'regular': 1.5,
+        'bold': 2,
+      },
+      control: {
+        type: 'radio'
+      }
+    },
+    size: {
+      name: 'Size',
+      control: {
+        type: 'range',
+        min: 20,
+        max: 60,
+        step: 2
+      }
+    },
+    type: {
+      table: { disable: true }
+    }
+  }
 }
 
 export const Icon = args => ({
   components: { EpIcon },
   setup() {
-    return { args }
+    const styles = computed(() => {
+      return {
+        '--ep-icon-width': `${args.size}px`,
+        '--ep-icon-height': `${args.size}px`,
+        '--ep-icon-color': args.color,
+        '--ep-icon-stroke-width': args.weight,
+      }
+    })
+
+    return { args, styles }
   },
-  template: '<ep-icon v-bind="args" />'
+  template: '<ep-icon v-bind="args" :style="styles" />'
 })
 
 Icon.args = {
