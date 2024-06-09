7de95ea8ab2fc8a4737ed4bd1baa6403bbe877d4
Eric Pitcock <accounts+git@ericpitcock.com>
2024-06-09 12:53:02 -0700

    composable index

---
+++ packages/epicenter-vue-components/src/composables/index.js
@@ -0,0 +1,16 @@
+//table
+import useColumnFilters from '../components/table/useColumnFilters.js'
+import useDataFilters from '../components/table/useDataFilters.js'
+import useExclude from '../components/table/useExclude.js'
+import usePagination from '../components/table/usePagination.js'
+import useSearch from '../components/table/useSearch.js'
+import useSorting from '../components/table/useSorting.js'
+
+export {
+  useColumnFilters,
+  useDataFilters,
+  useExclude,
+  usePagination,
+  useSearch,
+  useSorting,
+}
