669ef6155dcda17de1def8c497cd9f21f34e0ef5
Eric Pitcock <accounts+git@ericpitcock.com>
2024-12-24 12:07:53 -0800

    added itemCount component

---
+++ storybook/stories/components/EpItemCount.stories.js
@@ -0,0 +1,43 @@
+import { centeredSurface } from '../../helpers/decorators.js'
+
+import EpItemCount from '@/components/item-count/EpItemCount.vue'
+
+export default {
+  title: 'Components/Item Count',
+  component: EpItemCount,
+  decorators: [centeredSurface],
+  argTypes: {
+    count: {
+      name: 'Count',
+      control: {
+        type: 'number'
+      },
+    },
+    singular: {
+      name: 'Singular',
+      control: {
+        type: 'text'
+      },
+    },
+    plural: {
+      name: 'Plural',
+      control: {
+        type: 'text'
+      },
+    },
+  }
+}
+
+export const ItemCount = args => ({
+  components: { EpItemCount },
+  setup() {
+    return { args }
+  },
+  template: '<ep-item-count v-bind="args" />'
+})
+
+ItemCount.args = {
+  count: 2,
+  singular: 'Taco',
+  plural: 'Tacos'
+}
