c8c04316b753c3938887cfd0f48f31c4edffd6b1
Eric Pitcock <accounts+git@ericpitcock.com>
2024-12-29 06:50:58 -0800

    icons package cleanup

--- packages/epicenter-icons/index.js
+++ packages/epicenter-icons/index.js
@@ -7,28 +7,4 @@ const loadIcon = async (name) => {
   }
 }
 
-// array of icon names
-
 export { loadIcon }
-
-// Dynamically import all SVG files
-// const iconFiles = import.meta.glob('./icons/*.svg', { as: 'raw' })
-
-// // Generate an array of icon names (without extensions)
-// const icons = Object.keys(iconFiles).map((filePath) => {
-//   return filePath.split('/').pop().replace('.svg', '') // Extract file name without extension
-// })
-
-// // Function to load an icon dynamically
-// const loadIcon = async (name) => {
-//   try {
-//     const { default: rawSvg } = await iconFiles[`./icons/${name}.svg`]()
-//     return rawSvg
-//   } catch (error) {
-//     console.error(`Failed to load icon: ${name}`, error)
-//     return null
-//   }
-// }
-
-// // Export the icon loader and icon names
-// export { loadIcon, icons }
