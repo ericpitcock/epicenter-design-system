cc5aea62f39b27807ed23f4e52f0a474818f0833
Eric Pitcock <accounts+git@ericpitcock.com>
2024-12-29 06:49:30 -0800

    stopped using yalc for icons package in vue components, using vite alias to get icons from the package directly sorted out svgLoader dependency vite CSS options

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,31 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react'
+import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // build: {
   //   target: 'esnext' //browsers can handle the latest ES features
   // },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    }
+  },
   plugins: [
     vue(),
-    react()
+    react(),
+    svgLoader(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./packages/epicenter-vue-components/src', import.meta.url)),
-      // temporary workaround until this is a proper package
-      'epicenter-icons': fileURLToPath(new URL('./packages/epicenter-icons/index.js', import.meta.url)),
+      // use icons directly from source
+      '@ericpitcock/epicenter-icons': fileURLToPath(new URL('./packages/epicenter-icons/index.js', import.meta.url)),
     }
   },
 })
