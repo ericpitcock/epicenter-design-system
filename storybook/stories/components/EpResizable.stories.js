2f3b9c2cb8a8131d54223a4362578ec9c226cf69
Eric Pitcock <accounts+git@ericpitcock.com>
2025-01-31 12:38:51 -0800

    resize story prog

--- storybook/stories/components/EpResizable.stories.js
+++ storybook/stories/components/EpResizable.stories.js
@@ -1,29 +1,19 @@
 import EpResizable from '@/components/resizable/EpResizable.vue'
 import { ref } from 'vue'
 
-// const containerDecorator = () => {
-//   return {
-//     template: '<div style="height: 100vh; padding: 30px; display: flex;"><story/></div>'
-//   }
-// }
-
 export default {
   title: 'Components/Resizable',
   component: EpResizable,
-  // decorators: [containerDecorator],
   argTypes: {
     dragEdge: {
       control: { type: 'select' },
       options: ['top', 'right', 'bottom', 'left'],
-      description: 'Determines which edge is draggable for resizing.'
     },
     initialSize: {
       control: { type: 'text' },
-      description: 'Initial flex-basis value (e.g., "300px", "40%").'
     },
     minSize: {
       control: { type: 'number' },
-      description: 'Minimum size of the resizable container.'
     },
     maxSize: {
       control: { type: 'number' },
@@ -48,10 +38,10 @@ export const Resizable = args => ({
       @resize="newSize => console.log('Resized to:', newSize)"
     >
       <template #resizable>
-        <div style="background: lightgray; width: 100%; height: 100%;">Resizable Panel</div>
+        <div style="display: grid; place-items: center; width: 100%; height: 100%; background: lightgray; color: black;">Resizable Panel</div>
       </template>
       <template #sibling>
-        <div style="background: whitesmoke; height: 100%;">Sibling Panel</div>
+        <div style="display: grid; place-items: center; width: 100%; height: 100%; background: whitesmoke; color: black;">Sibling Panel</div>
       </template>
     </ep-resizable>
   `
@@ -60,6 +50,6 @@ export const Resizable = args => ({
 Resizable.args = {
   dragEdge: 'right',
   initialSize: '50%', // percentage or pixel value
-  // minSize: 200,
-  // maxSize: 600,
+  minSize: 200,
+  maxSize: 600,
 }
