c01498011f4450d3f42e8176d0a0a5c457b83435
Eric Pitcock <accounts+git@ericpitcock.com>
2025-11-14 14:41:02 -0800

    optimizations

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,19 +2,51 @@
 
 const config = {
   stories: [
-    '../storybook/stories/**/*.mdx', // docs
+    // '../storybook/stories/**/*.mdx', // docs
     '../storybook/stories/**/*.stories.js'
   ],
+
   addons: [
     '@storybook/addon-controls',
-    '@storybook/addon-docs',
+    // '@storybook/addon-docs',
     '@storybook/addon-toolbars',
-    '@storybook/addon-a11y',
+    // '@storybook/addon-a11y',
   ],
+
   staticDirs: ['../static'],
+
   framework: {
     name: '@storybook/vue3-vite',
     options: {},
   },
+
+  viteFinal: async (config) => {
+    return {
+      ...config,
+      resolve: { ...(config.resolve ?? {}), preserveSymlinks: true },
+      server: {
+        ...(config.server ?? {}),
+        fs: { ...(config.server?.fs ?? {}), strict: true },
+        watch: {
+          ignored: [
+            '**/node_modules/**',
+            '**/.git/**',
+            '**/dist/**',
+            '**/storybook-static/**',
+            '**/docs/.vitepress/dist/**',
+            '**/.cache/**',
+            '**/.storybook-cache/**',
+            '../static/**',
+          ],
+          usePolling: false,
+        },
+      },
+      optimizeDeps: { include: ['vue', '@vueuse/core'] },
+    }
+  },
+
+  core: {
+    disableWhatsNewNotifications: true
+  }
 }
 export default config
