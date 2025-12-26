e39db3ee9696b14997ee27acb18c95f6ca18201d
Eric Pitcock <accounts+git@ericpitcock.com>
2025-12-26 14:22:06 -0800

    lint updates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,19 +3,58 @@ module.exports = {
   root: true,
   extends: [
     'plugin:vue/vue3-recommended',
-    // react plugin
     'plugin:react/recommended',
     'eslint:recommended',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
   },
-  plugins: ['import', 'simple-import-sort'],
+  plugins: ['import', 'perfectionist'],
   rules: {
+    // Existing rules
     'no-unused-vars': ['error', { 'varsIgnorePattern': '^props$' }],
-    // Enforce a specific order for imports
-    'import/order': 'off', // Disable this in favor of simple-import-sort
-    'simple-import-sort/imports': 'warn', // Sort imports in a logical order
-    'simple-import-sort/exports': 'warn', // Sort exports if multiple
+
+    // Perfectionist - Import sorting
+    'perfectionist/sort-imports': ['warn', {
+      type: 'natural',
+      order: 'asc',
+      groups: [
+        'builtin',
+        'external',
+        'internal',
+        'parent',
+        'sibling',
+        'index',
+      ],
+    }],
+
+    // Perfectionist - Interface/Type sorting
+    'perfectionist/sort-interfaces': 'error',
+
+    // Perfectionist - Explicitly disabled (preserve logical grouping)
+    'perfectionist/sort-objects': 'off',
+    'perfectionist/sort-variable-declarations': 'off',
+
+    // Vue - Block order
+    'vue/block-order': ['error', {
+      order: ['script', 'template', 'style'],
+    }],
+
+    // Vue - Macro order
+    'vue/define-macros-order': ['error', {
+      order: ['defineProps', 'defineEmits', 'defineSlots', 'defineModel'],
+    }],
+
+    // Vue - Enforce script-setup
+    'vue/component-api-style': ['error', ['script-setup']],
+
+    // Vue - Enforce type-based props
+    'vue/define-props-declaration': ['error', 'type-based'],
+
+    // Vue - Padding between blocks
+    'vue/padding-line-between-blocks': ['error', 'always'],
+
+    // Cleanup - Remove old simple-import-sort
+    'import/order': 'off',
   },
 }
